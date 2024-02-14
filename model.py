8ed00ca9b8f2a8f2be0b766a166c746df09775e9
Rory Williams <rorywilliams183@gmail.com>
2024-02-14 17:46:13 +0000

    extract face region from detector

--- model.py
+++ model.py
@@ -7,14 +7,34 @@ from utils import tools
 
 class ActiveSpeaker():
     def __init__(self, frame):
-        self.frame = frame
+        self.frame = frame.numpy()
 
     def model(self):
         # for frame in enumerate(train_features):
-        frame = self.frame.numpy()
-        face_detect = FaceDetection(frame)
+        face_detect = FaceDetection(self.frame)
         faces = face_detect.detect()
 
-        height, width = frame.shape[:2]
-        # print(faces)
-        tools.plot_faces_detected(frame, faces)
+        self.lip_detection(faces)
+        # tools.plot_faces_detected(self.frame, faces)
+        
+
+        return faces
+
+ 
+    def lip_detection(self, faces):
+        h, w = self.frame.shape[:2]
+        for face in faces:
+            # Gets coordinates of bounding box
+            x1, y1, x2, y2 = face[3:7] * h
+
+            # Grabs extra pixels around box to account for errors and also check ranges
+            x1 = max(round(float(x1))-5, 0)
+            y1 = max(round(float(y1))-5, 0)
+            x2 = min(round(float(x2))+5, w)
+            y2 = min(round(float(y2))+5, h)
+
+            # Extracts and resizes the face detected from the frame
+            face_region = cv2.resize(self.frame[y1:y2, x1:x2], (64,64))
+            # tools.plot_frame(face_region)
+
+
