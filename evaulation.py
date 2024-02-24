5ed7e5ff36d71f11a85d09415992c28d7bc63b69
Rory Williams <rorywilliams183@gmail.com>
2024-02-24 16:26:28 +0000

    added very messy but basic speaker evaluation function

--- evaulation.py
+++ evaulation.py
@@ -43,5 +43,53 @@ def eval_face_detection(predicteds, actuals):
             
     return count, total
 
-# if __name__ == "__main__":
 
+def evaluate(prediction, actual):
+    if torch.is_tensor(actual[1]):
+        a_faces = actual[1].numpy()
+    else:
+        a_faces = actual[1]
+
+    a_label = actual[-1]
+    p_faces = prediction['faces']
+    p_labels = prediction['label']
+    t_correct = 0
+    total = 0
+
+    if len(p_labels) == 0 or len(p_faces) == 0:
+        if len(a_faces.shape) > 1:
+            return len(a_faces), 0 
+        return 1, 0
+
+    if len(a_faces.shape) > 1:
+        for i in range(len(a_faces)):
+            for j in range(len(p_faces)):
+                if face_evaluate(p_faces[j], a_faces[i]):
+                    if p_labels[j] == a_label[i]:
+                        t_correct += 1
+                        break
+
+            total += 1
+    else:
+        for j in range(len(p_faces)):
+            if face_evaluate(p_faces[j], a_faces):
+                if p_labels[j] == a_label:
+
+                    t_correct += 1
+                    break
+        total += 1
+
+
+    return total, t_correct
+
+
+
+
+def face_evaluate(prediction, actual):
+    x1, y1, x2, y2 = prediction * 300
+    a_x1, a_y1, a_x2, a_y2 = actual * 300
+    c_x = (a_x1 + a_x2) / 2
+    c_y = (a_y1 + a_y2) / 2
+    if (c_x >= x1 and c_x <= x2) and (c_y >= y1 and c_y <= y2):
+        return True
+    return False
