ed1f0dec4aaa066cc604254bfe9a4fc96ac5c3fc
Rory Williams <rorywilliams183@gmail.com>
2024-02-24 16:25:38 +0000

    added speaker classification using the angel between lower and centre lip

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import cv2, dlib
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from imutils import face_utils
 from faceDetection.faceDetector import FaceDetection
@@ -17,71 +18,84 @@ class ActiveSpeaker():
         # for frame in enumerate(train_features):
         face_detect = FaceDetection(self.frame)
         faces = face_detect.detect()
+        predicted = {'faces' : [], 'label' : []}
+        for face in faces:
+            face_region, lip_pixels = self.feature_detection(face)
+            # tools.plot_faces_detected(self.frame, faces)
 
-        face_region, lip_pixels = self.lip_detection(faces)
-        # tools.plot_faces_detected(self.frame, faces)
+            # Set lip pixels values
+            speaking = 'NOT_SPEAKING' 
+            if len(lip_pixels) > 0 and len(face_region) > 0:
+                centre_upper = lip_pixels[3]
+                centre_lower = lip_pixels[9]
+                left = lip_pixels[0]
+                right = lip_pixels[6]
 
-        # Set lip pixels values 
-        if len(lip_pixels) > 0:
-            centre_upper = lip_pixels[3]
-            centre_lower = lip_pixels[9]
-            left = lip_pixels[0]
-            right = lip_pixels[6]
-            # cv2.circle(face_region, (left[0], left[1]), 1, (255,0,0), -1)
-            # cv2.circle(face_region, (centre_lower[0], centre_lower[1]), 1, (255,0,0), -1)
-            # cv2.circle(face_region, (right[0], right[1]), 1, (255,0,0), -1)
-            # cv2.circle(face_region, (centre_upper[0], centre_upper[1]), 1, (255,0,0), -1)
+                # Sets bounding box for lips
+                lip_box = [left[0]-2, centre_upper[1]-2, right[0]+2, centre_lower[1]+2]
 
-            # tools.plot_frame(face_region)
+                # Get area of lips from face
+                lip_area = face_region[lip_box[1]:lip_box[3], lip_box[0]:lip_box[2]]
 
-            # Sets bounding box for lips
-            lip_box = [left[0]-2, centre_upper[1]-2, right[0]+2, centre_lower[1]+2]
-            # Get area of lips from face
-            lip_area = face_region[lip_box[1]:lip_box[3], lip_box[0]:lip_box[2]]
-            self.speaker_detection(face_region, lip_box, lip_area)
-            tools.plot_box(face_region, lip_box)
-            #tools.plot_frame(lip_area)
+                speaking = self.speaker_detection(face_region, lip_pixels, lip_area, lip_box)
 
-        return faces
+                # cv2.circle(face_region, (left[0], left[1]), 1, (255,0,0), -1)
+                # cv2.circle(face_region, (centre_lower[0], centre_lower[1]), 1, (255,0,0), -1)
+                # cv2.circle(face_region, (right[0], right[1]), 1, (255,0,0), -1)
+                # cv2.circle(face_region, (centre_upper[0], centre_upper[1]), 1, (255,0,0), -1)
 
-    def speaker_detection(self, face_region, lip_pixels, lip_area):
-        # lip_area = cv2.resize(lip_ar)
-        # lip_gray = cv2.cvtColor(lip_area, cv2.COLOR_BGR2GRAY)
-        # tools.plot_frame(lip_area)
-        pass
+                predicted['faces'].append(face[3:7])
+                predicted['label'].append(speaking)
+                # tools.plot_box(face_region, lip_box)
+                # tools.plot_frame(lip_area)
+
+        return predicted
+
+    def speaker_detection(self, face_region, lip_pixels, lip_area, lip_box):
+        centre_lip = ((lip_box[0] + lip_box[2])/2, (lip_box[1] + lip_box[-1])/2)
+        centre_upper = lip_pixels[3]
+        centre_lower = lip_pixels[9]
+        left = lip_pixels[0]
+        right = lip_pixels[6]
+
+        opposite = abs(centre_lower[1] - centre_lip[1])
+        hypotenuse = np.linalg.norm(centre_lip - centre_lower)
+        if opposite <= hypotenuse and opposite > 0:
+            if np.arcsin(opposite / hypotenuse) > 1:
+                return 'SPEAKING'
+        return 'NOT_SPEAKING'
 
     def kalman(self):
         pass
 
     # Gets lips using dlib shape predictor model
-    def lip_detection(self, faces):
+    def feature_detection(self, face):
         h, w = self.frame.shape[:2]
         H = 64
-        for face in faces:
-            # Gets coordinates of bounding box
-            x1, y1, x2, y2 = face[3:7] * h
+        # Gets coordinates of bounding box
+        x1, y1, x2, y2 = face[3:7] * h
 
-            # Grabs extra pixels around box to account for errors and also check ranges
-            x1 = max(round(float(x1))-5, 0)
-            y1 = max(round(float(y1))-5, 0)
-            x2 = min(round(float(x2))+5, w)
-            y2 = min(round(float(y2))+5, h)
+        # Grabs extra pixels around box to account for errors and also check ranges
+        x1 = max(round(float(x1))-5, 0)
+        y1 = max(round(float(y1))-5, 0)
+        x2 = min(round(float(x2))+5, w)
+        y2 = min(round(float(y2))+5, h)
 
-            # Extracts and resizes the face detected from the frame
-            face_region = cv2.resize(self.frame[y1:y2, x1:x2], (H,H))
-            gray = cv2.cvtColor(face_region, cv2.COLOR_BGR2GRAY)
-  
-            points = landmarks(gray, dlib.rectangle(0, 0, H, H))
-            points = face_utils.shape_to_np(points)
+        # Extracts and resizes the face detected from the frame
+        face_region = cv2.resize(self.frame[y1:y2, x1:x2], (H,H))
+        gray = cv2.cvtColor(face_region, cv2.COLOR_BGR2GRAY)
 
-            # for i in range(48,68): 
-            #     # draw the keypoints on the detected faces
-            #     cv2.circle(face_region, (points[i][0], points[i][1]), 1, (0, 255, 0), -1)
+        points = landmarks(gray, dlib.rectangle(0, 0, H, H))
+        points = face_utils.shape_to_np(points)
 
-            # tools.plot_frame(face_region)
-            #tools.plot_frame(lip_pixels)
-            #tools.plot_color_space(face_region)
-            #tools.plot_hist(face_region)
+        # for i in range(48,68): 
+        #     # draw the keypoints on the detected faces
+        #     cv2.circle(face_region, (points[i][0], points[i][1]), 1, (0, 255, 0), -1)
+
+        # tools.plot_frame(face_region)
+        #tools.plot_frame(lip_pixels)
+        #tools.plot_color_space(face_region)
+        #tools.plot_hist(face_region)
 
         try:
             return face_region, points[48:-1]
