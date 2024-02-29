5c7212677c1b1e3c9c60530120fcdf4c1c4bce52
Rory Williams <rorywilliams183@gmail.com>
2024-02-29 19:28:27 +0000

    update default thresh

--- faceDetection/faceDetector.py
+++ faceDetection/faceDetector.py
@@ -12,7 +12,7 @@ path = os.getcwd()
 path = os.path.join(path, 'faceDetection/model')
 
 class FaceDetection():
-    def __init__(self, frame, threshold: float = 0.27, model: str = 'res10_300x300_ssd_iter_140000.caffemodel'):
+    def __init__(self, frame, threshold: float = 0.25, model: str = 'res10_300x300_ssd_iter_140000.caffemodel'):
         self.frame = frame
         self.face_detector = cv2.dnn.readNetFromCaffe(f"{path}/deploy.prototxt.txt", f"{path}/{model}")
         self.threshold = threshold
@@ -24,18 +24,18 @@ class FaceDetection():
 
         # Performs forward pass on the frame and returns array of detected faces
         # returns array in shape (1, 1, num_faces_detected, 7)
-        # where we are interested in last 7 = [_, _, confidence_score, x1, y1, x2, y2]
+        # where we are interested in last subarray for every face detected = [_, _, confidence_score, x1, y1, x2, y2]
         # coordinates are normalised to frame size 
         faces = self.face_detector.forward()
         num_faces = faces.shape[2]
 
         # Removes faces detected with low confidence values
         # Adds the high confidence faces to a new array conf_faces
-        conf_faces = []
+        confident_faces = []
         for i in range(num_faces):
             confidence = faces[0, 0, i, 2]
 
             if confidence > self.threshold:     
-                conf_faces.append(faces[0, 0, i, :])
+                confident_faces.append(faces[0, 0, i, :])
 
-        return np.array(conf_faces)
+        return np.array(confident_faces)
