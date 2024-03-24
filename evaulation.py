ac73534effe0a5b469fd40dccefa9e11665d0397
Rory Williams <rorywilliams183@gmail.com>
2024-03-24 15:40:04 +0000

    clean up of the face detection evaluation function

--- evaulation.py
+++ evaulation.py
@@ -14,7 +14,7 @@ Compare predicted labels with actual labels
 
 # General evaluation for face detection
 def eval_face_detection(predicteds, actuals):
-    count = 0
+    correct = 0
     if len(predicteds) == 0 or len(actuals) == 0:
         if not torch.is_tensor(actuals[1]):
             return 0, len(actuals[1])
@@ -24,29 +24,22 @@ def eval_face_detection(predicteds, actuals):
         for prediction in predicteds:
             x1, y1, x2, y2 = prediction[3:7] * 300
             for actual in actuals[1]:
-                a_x1, a_y1, a_x2, a_y2 = actual * 300
-                c_x = (a_x1 + a_x2) / 2
-                c_y = (a_y1 + a_y2) / 2
-                if (c_x >= x1 and c_x <= x2) and (c_y >= y1 and c_y <= y2):
-                    count += 1
-                    break
+                if face_evaluate(prediction, actual):
+                    correct += 1
 
         total = len(actuals[1])
 
     else:
         for prediction in predicteds:
-            x1, y1, x2, y2 = prediction[3:7] * 300
-            a_x1, a_y1, a_x2, a_y2 = actuals[1] * 300
-            c_x = (a_x1 + a_x2) / 2
-            c_y = (a_y1 + a_y2) / 2
-            if (c_x >= x1 and c_x <= x2) and (c_y >= y1 and c_y <= y2):
-                count += 1
+            if face_evaluate(prediction, actual):
+                correct += 1
 
         total = 1
             
     return count, total
 
 
+# Evaluation function for the prediction and actual label
 def evaluate(prediction, actual):
     tp = 0
     fp = 0
@@ -116,11 +109,14 @@ def label_eval(prediction, actual, counts):
 # Calculates evaluation metrics from the give counts
 def metrics(counts):
     tp,fp,tn,fn = counts
+
+    # Catch dividing by 0
     if tp+fp == 0:
         precision = 0
     else:
         precision = tp / (tp+fp)
 
+    # Catch dividing by 0
     if tp+fn == 0:
         recall = 0
     else:
