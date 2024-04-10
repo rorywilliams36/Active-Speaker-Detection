5bb7d3c0d259a7a9bef221e27b3ba6200fa73be8
Rory Williams <rorywilliams183@gmail.com>
2024-04-10 16:36:36 +0100

    added searches to tune params

--- model.py
+++ model.py
@@ -2,16 +2,21 @@ import joblib
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import log_loss, hinge_loss
+from scipy.stats import expon
 
 class SVM():
     def __init__(self, load, model_path):
         if load:
-            self.model = self.load_parameters(None, load_previous)
+            self.model = self.load_parameters(None)
         elif model_path is not None:
-            self.model = self.load_parameters(model_path, False)
+            self.model = self.load_parameters(model_path)
         else:
-            self.model = svm.NuSVC(gamma="auto", kernel='rbf', probability=True, class_weight={0 : 0.6})
+            # self.model = svm.NuSVC(gamma="auto", kernel='rbf', probability=True, class_weight={0 : 0.6})
+            param_grid = {'gamma' : expon(scale=0.1), 'nu' : expon(scale=0.1), 'kernel' : ['rbf']}
+            # self.model = GridSearchCV(svm.NuSVC(), param_grid, refit=True, verbose=3)
+            self.model = RandomizedSearchCV(svm.NuSVC(), param_distributions=param_grid, n_iter=100, refit=True, verbose=3)
 
     # Trains model
     def train(self, X, Y):
@@ -44,24 +49,23 @@ class SVM():
     # Function to save the parameters of the model
     def save_parameters(self, params):
         try:
-            joblib.dump(params,"models/svm_parameters.pkl")
+            with open('wb', "models/svm_parameters.pkl") as file:
+                joblib.dump(params, file)
         except:
             print('Error occured when saving model')
 
     # loads presaved model
-    def load_parameters(self, path, prev):
+    def load_parameters(self, path):
         try:
-            if (path is not None) and (not prev):
-                params = joblib.load(path)
+            if path is not None:
+                with open('rb', path) as file:
+                    return joblib.load(file)
             else:
-                params = joblib.load("models/svm_parameters.pkl")
-            return params
+                with open('rb', "models/svm_parameters.pkl") as file:
+                    return joblib.load(file)
         except:
             print('Error loading saved model. Train first')
             quit()
-
-    def load_data(self):
-        pass
 
     # Saves the predicted results to a new file
     def save_results(self, X, Y):
